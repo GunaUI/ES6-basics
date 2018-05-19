fd58a13d360488e90f32c86448ee36f9b4f70c73
gunm <gunm@cisco.com>
2018-05-19 11:12:21 +0530

    Let Vs Constant

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // Diff between VAR and LET
 
 // Let is based on block scope where is VAR is based on functional scope.
+
+// Diff btwn LET and CONST
+
+    // Let can be reinitailised
+    // Const cannot be reinitialised
+    // In constant we cannot change the object but we can change the properties...
+
